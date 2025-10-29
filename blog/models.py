6a1158409a0b036450718d8fb25317826dd8356c
nyutdf2 <annslob@gmail.com>
2025-10-29 16:49:16 +0300

    move code to reusable QuerySet method

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models import Count
 
+
+class PostQuerySet(models.QuerySet):
+    
+    def year(self, year):
+        posts_at_year = self.filter(published_at__year=year).order_by('published_at')
+        return posts_at_year
+    
+
+class TagQuerySet(models.QuerySet):
+    
+    def popular(self):
+        popular_tags = Tag.objects.annotate(Count('posts')).order_by('-posts__count')
+        return popular_tags
+       
 
 class Post(models.Model):
     title = models.CharField('Заголовок', max_length=200)
@@ -9,7 +24,8 @@ class Post(models.Model):
     slug = models.SlugField('Название в виде url', max_length=200)
     image = models.ImageField('Картинка')
     published_at = models.DateTimeField('Дата и время публикации')
-
+    objects = PostQuerySet.as_manager()
+    
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -39,6 +55,7 @@ class Post(models.Model):
 
 class Tag(models.Model):
     title = models.CharField('Тег', max_length=20, unique=True)
+    objects = TagQuerySet.as_manager()
 
     def __str__(self):
         return self.title
@@ -76,3 +93,4 @@ class Comment(models.Model):
         ordering = ['published_at']
         verbose_name = 'комментарий'
         verbose_name_plural = 'комментарии'
+        
